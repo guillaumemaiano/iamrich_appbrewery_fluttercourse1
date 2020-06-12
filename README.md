3a44cbff33f95b8b9d70d05849ed45bb06965423
Guillaume Maiano <guillaume@maiano.fr>
2020-06-12 16:53:02 +0200

    Initial very uninteresting commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# iamrichcourselevel1
+
+The app for level 1 of the Flutter course given out by Google in June 2020.
+
---
+++ android/app/src/main/kotlin/fr/maiano/iamrichcourselevel1/MainActivity.kt
@@ -0,0 +1,6 @@
+package fr.maiano.iamrichcourselevel1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
