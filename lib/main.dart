cf17cb146fd5c2f1ffe1e907d5b46190655d9f6a
Guillaume Maiano <guillaume@maiano.fr>
2020-06-12 17:37:38 +0200

    Woohoo simple app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('I am filthy rich')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title:
+                //   Center(
+                Text('I am filthy rich'),
+            //  ),
+            backgroundColor: Colors.deepOrange[700],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://cdnb.artstation.com/p/assets/images/images/000/745/459/large/yann-wissang-squig-render-scene1.jpg?1432074614'),
+            ),
+          ),
+          backgroundColor: Colors.deepOrange[900]),
+    ),
+  );
 }
